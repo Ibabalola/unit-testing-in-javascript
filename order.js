83bb9f2ebddf35637f786b0b1cc4df2698c528ce
Isaac Babalola <info@isaacbabalola.co.uk>
2020-08-02 18:43:51 +0100

    mocking continued

--- order.js
+++ order.js
@@ -1,32 +1,28 @@
-const someOrder = {
-    items: [
-        { name: 'item 1', quantity: 2, price: 200},
-        { name: 'item 2', price: 800 },
-        { name: 'item 3', quantity: 1, price: 100, shipping: true }
-    ]
-}
-
 // passing fetch inside of this function is a technique
 // called Dependancy Injection
-const orderTotal = (fetch, order) => {
+
+const orderTotal = async (fetch, order) => {   
     if (!order.userId) {
-        return Promise.reject('UserId unspecified');
+        throw new Error('UserId unspecified');
     }
 
-    fetch(`https://jsonplaceholder.typicode.com/todos/${order.userId}`);
+    return await fetch(`https://jsonplaceholder.typicode.com/todos/${order.userId}`)
+        .then(response => response.json())
+        .then(json => {
+            // || means optional value
+            const totalNormalItems = order.items
+                .filter(item => !item.shipping)
+                .reduce((prev, cur) => prev + (cur.quantity || 1) * cur.price * (json.rate || 1), 0);
+        
+            // !! means if (true) - boolean operator
+            const shippingItem = order.items.find(item => !!item.shipping);
+            const shipping = (shippingItem) ? totalNormalItems > 1000 ? 0 : shippingItem.price : 0;
 
-    // || means optional value
-    const totalNormalItems = order.items
-        .filter(item => !item.shipping)
-        .reduce((prev, cur) => prev + (cur.quantity || 1) * cur.price, 0);
-
-    // !! means if (true) - boolean operator
-    const shippingItem = order.items
-        .find(item => !!item.shipping);
-
-    const shipping = (shippingItem) ? totalNormalItems > 1000 ? 0 : shippingItem.price : 0;
-            
-    return Promise.resolve(totalNormalItems + shipping);
+            return totalNormalItems + shipping;
+        })
+        .catch(e => {
+            console.log('Error', e);
+        });
 };
 
 module.exports = orderTotal;
--- order.test.js
+++ order.test.js
@@ -1,60 +1,79 @@
 const orderTotal = require('./order');
-const emptyFunction = () => {};
+const emptyFunction = () => {
+    return Promise.resolve({
+        json: () => Promise.resolve({})
+    });
+};
 
 describe('Order Total', () => {
-    it('should return a total of 6', () => {
+    it('should return a total of 6', async () => {
         const someOrder = {
+            userId: 1,
             items: [
                 { name: 'item 1', quantity: 2, price: 3 }
             ]
         };
 
-        orderTotal(emptyFunction, someOrder)
-         .then(result => expect(result).toBe(6));
+        const result = await orderTotal(emptyFunction, someOrder);
+
+        expect(result).toBe(6);
     });
 
-    it('should return a total of 3', () => {
+    it('should return a total of 3', async () => {
         const someOrder = {
+            userId: 1,
             items: [
                 { name: 'item 1', price: 3 }
             ]
         };
 
-       orderTotal(emptyFunction, someOrder)
-        .then(result => expect(result).tobe(3));
+        const result = await orderTotal(emptyFunction, someOrder);
+
+        expect(result).toBe(3);
     });
 
-    it('should return a total of 808', () => {
+    it('should return a total of 808', async () => {
         const someOrder = {
+            userId: 1,
             items: [
                 { name: 'item 1', price: 8, quantity: 1 },
                 { name: 'item 2', price: 800, quantity: 1 }
             ]
         };
 
-        orderTotal(emptyFunction, someOrder)
-         .then(result =>  expect(emptyFunction, result).toBe(808));
+        const result = await orderTotal(emptyFunction, someOrder);
+
+        expect(result).toBe(808);
     });
 
-    it('should return a total of 60', () => {
+    it('should return a total of 60', async () => {
         const someOrder = {
+            userId: 1,
             items: [
                 { name: 'item 1', price: 20, quantity: 1 },
                 { name: 'item 2', price: 40, quantity: 1 }
             ]
         };
 
-        orderTotal(emptyFunction, someOrder)
-         .then(result => expect(emptyFunction, result).toBe(60));
+        const result = await orderTotal(emptyFunction, someOrder);
+
+        expect(result).toBe(60);
     });
 })
 
 describe('API calls', () => {
-    it('should make an API call', () => {
+    it('should make an API call', async () => {
         let isFakeFetchCalled = false;
         const fakeFetch = url => {
             expect(url).toBe('https://jsonplaceholder.typicode.com/todos/1');
-            isFakeFetchCalled ; true
+            isFakeFetchCalled = true;
+            return Promise.resolve({
+                json: () => Promise.resolve({
+                    id: 1,
+                    title: 'this is a title',
+                    rate: 5
+                })
+            });
         }
 
         const someOrder = {
@@ -64,17 +83,15 @@ describe('API calls', () => {
             ]
         };
 
-        orderTotal(fakeFetch, someOrder)
-            .then(result => {
-                expect(isFakeFetchCalled).toBe(true)
-            });
+        const result = await orderTotal(fakeFetch, someOrder);
+        expect(isFakeFetchCalled).toBe(true);
+        expect(result).toEqual(30);
     });
 
-    it('should reject if userId does not exists', () => {
+    it('should reject if userId does not exists', async () => {
         let isFakeFetchCalled = false;
         const fakeFetch = url => {
-            expect(url).toBe('https://jsonplaceholder.typicode.com/todos/1');
-            isFakeFetchCalled ; true
+            isFakeFetchCalled = true
         }
 
         const someOrder = {
@@ -83,7 +100,8 @@ describe('API calls', () => {
             ]
         };
 
-        expect(orderTotal(fakeFetch, someOrder)).rejects.toThrow('UserId unspecified');
+        await expect(orderTotal(fakeFetch, someOrder)).rejects.toThrow('UserId unspecified');
+        expect(isFakeFetchCalled).toBe(false);
     });
 
 });
